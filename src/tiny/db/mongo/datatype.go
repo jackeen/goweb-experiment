55ea1d622b0b9cae17f95f64a21bf4257e4cb2ff
zhangjia <sxzhangjia@gmail.com>
2014-07-20 01:02:07 +0800

    data type

--- src/tiny/db/mongo/datatype.go
+++ src/tiny/db/mongo/datatype.go
@@ -1,23 +1,46 @@
 package mongo
 
+type IdNum struct {
+	Post      int
+	Cate      int
+	Tag       int
+	User      int
+	UserGroup int
+}
+
 type Post struct {
-	Id      string
-	Title   string
-	Content string
-	Auth    string
-	AddDate string
+	Id           int
+	Title        string
+	Content      string
+	Auth         int
+	Cate         int
+	Tags         []int
+	CreateDate   string
+	LastEditDate string
+	EditState    bool
 }
 
 type Cate struct {
-	Id       string
+	Id       int
 	Name     string
 	Explain  string
-	Children []string
-	Parent   string
+	Children []int
+	Parent   int
 }
 
 type Tag struct {
-	Id      string
+	Id      int
 	Name    string
 	Explain string
+}
+
+type UserGroup struct {
+}
+
+type User struct {
+	Id          int
+	Name        string
+	Nick        string
+	CreateDate  string
+	StateNumber int
 }
