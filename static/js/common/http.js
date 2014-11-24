7b1c8722a12c52cb1d4e32756236d598819ea599
jia zhang <sxzhangjia@gmail.com>
2014-11-25 00:32:11 +0800

    ajax module

--- static/js/common/http.js
+++ static/js/common/http.js
@@ -2,13 +2,26 @@ define(function(){
 
 	
 
-	function ajax(){
-		var xhr = new XMLHttpRequest()
-
+	function ajax(conf, onchange){
+		
+		var xhr = new XMLHttpRequest();
+		xhr.open(conf.type, conf.url, conf.async);
+		xhr.send(conf.data);
+		xhr.onreadystatechange = onchange;
+		return xhr;
 	}
 
 	function get(){
-
+		var xhr = ajax({
+			url: url,
+			type: 'get',
+			async: true
+		}, function(e){
+			var t = e.target;
+			if(t.status === 4 && t.state == 200){
+				
+			}
+		});
 	}
 
 	function post(){
