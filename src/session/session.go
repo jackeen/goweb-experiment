2b3207caa88ca7deba0f8f7555c1750ed61e0f03
jia zhang <sxzhangjia@gmail.com>
2014-11-28 00:06:02 +0800

    uuid

--- src/session/session.go
+++ src/session/session.go
@@ -1,14 +1,32 @@
 package session
 
 import (
+	"crypto/md5"
+	"encoding/hex"
+	"io"
+	"math/rand"
+	"strconv"
 	"time"
 )
 
+func MD5(s string) string {
+	m := md5.New()
+	io.WriteString(m, s)
+	return hex.EncodeToString(m.Sum(nil))
+}
+
+func UUID() string {
+	nano := time.Now().UnixNano()
+	rand.Seed(nano)
+	randNum := rand.Int63()
+	id := MD5(strconv.FormatInt(randNum, 16))
+	return id
+}
+
 type Session struct {
 	Id     string
-	Name   string
-	Expire time.Time
-	Datas  map[string]interface{}
+	Expire int
+	Data   map[string]interface{}
 }
 
 func (self *Session) setData(k string, v interface{}) {
