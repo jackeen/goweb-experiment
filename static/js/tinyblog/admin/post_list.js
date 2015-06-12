0066de6b91641265b42464d7b25743640e3017c3
Jia zhang <sxzhangjia@gmail.com>
2015-06-12 18:41:39 +0800

    admin post list fetch api

--- static/js/tinyblog/admin/post_list.js
+++ static/js/tinyblog/admin/post_list.js
@@ -4,11 +4,33 @@ define({
 
 }, function(global, modules){
 
-	fetch("/api/postlist/get").then(function (res){
-		console.log(res);
-		window.res = res;
-	}, function (e){
-		console.log(e);
+	const postListURL = "/api/postlist/get"
+
+	function buildPostList(list) {
+
+	}
+
+	fetch(postListURL, {
+		method: "get"
+	}).then(function (res) {
+
+		return res.json();
+
+	}, function (err) {
+
+		console.log("network:", err);
+
+	}).then(function (d) {
+
+		if (d.state) {
+			buildPostList(d.data)
+			console.log("all post count: ", d.count);
+		} else {
+			console.log(d.message);
+		}
+
+	}).catch(function (err) {
+		console.log(err);
 	});
 
 });
