d71003b8fda28235adfad94d40d4b101c4916660
jia zhang <sxzhangjia@gmail.com>
2015-06-15 08:17:02 +0800

    admin post list build

--- static/js/tinyblog/admin/post_list.js
+++ static/js/tinyblog/admin/post_list.js
@@ -4,10 +4,23 @@ define({
 
 }, function(global, modules){
 
+	"use strict"
+
 	const postListURL = "/api/postlist/get"
 
 	function buildPostList(list) {
+		
+		var listStr = "", item = {};
+		var len = list.length;
 
+		for (var i = 0; i<len; i++) {
+			item = list[i];
+			listStr += `<li><h2>${item.title}</h2>\
+							<p>${item.createTime}-${item.author}</p>\
+						</li>`;
+		}
+
+		return listStr;
 	}
 
 	fetch(postListURL, {
@@ -16,21 +29,19 @@ define({
 
 		return res.json();
 
-	}, function (err) {
-
-		console.log("network:", err);
-
 	}).then(function (d) {
-
+ 
 		if (d.state) {
-			buildPostList(d.data)
+			let listCon = document.querySelector("#postlist");
+			listCon.innerHTML = buildPostList(d.data);
 			console.log("all post count: ", d.count);
 		} else {
-			console.log(d.message);
+			return Error(d.message);
 		}
 
 	}).catch(function (err) {
-		console.log(err);
+		alert(err);
 	});
+
 
 });
